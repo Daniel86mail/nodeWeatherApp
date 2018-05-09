3693c974b45fcb96ee691c147f5fde449e17508a
dciuraru <dciuraru@xsightsys.com>
2018-05-09 14:42:08 +0300

    testing forecast.io api

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-    .options({
-        a: {
-                demand: true,
-                alias: 'address',
-                describe: 'Address to fetch weather for',
-                string: true //parse input as string
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const argv = yargs
+//     .options({
+//         a: {
+//                 demand: true,
+//                 alias: 'address',
+//                 describe: 'Address to fetch weather for',
+//                 string: true //parse input as string
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if(errorMessage){
-        console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if(errorMessage){
+//         console.log(errorMessage);
+//     }
+//     else{
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+const request = require('request');
+
+request({
+    url: `https://api.forecast.io/forecast/f648e9f1d20496f6cc4979508ef4a36c/32.0852999,34.78176759999999`,
+    json: true
+}, 
+(error, response, body) => {
+    if(!error && response.statusCode === 200){
+        console.log(body.currently.temperature);
+        console.log(body.currently.summary);
     }
-    else{
-        console.log(JSON.stringify(results, undefined, 2));
+    else {
+        console.log('Unable to fetch weather');
     }
+
 });
+
+//forecast.io api key: f648e9f1d20496f6cc4979508ef4a36c
+//https://api.forecast.io/forecast/f648e9f1d20496f6cc4979508ef4a36c/32.0852999,34.78176759999999
