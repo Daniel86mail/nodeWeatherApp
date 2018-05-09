b0676a1f1fe294c3716214233dbdec502a34c3ef
dciuraru <dciuraru@xsightsys.com>
2018-05-09 11:36:32 +0300

    sec 4, 28

---
+++ app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyC8pwwyrSBa-RJ1DL_ZyEkQJ14MISqGgA0',
+    json: true
+}, 
+(error, response, body) => {
+    console.log(body);
+});
